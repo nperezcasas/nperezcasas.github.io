93a6a536405126b93dd31165bae05e2b60561261
nperezcasas <nupc20@gmail.com>
2022-10-31 11:32:14 -0500

    10312022

--- blc/js/plugins.js
+++ blc/js/plugins.js
@@ -22,3 +22,18 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
