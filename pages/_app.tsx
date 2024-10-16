58258698db34165073b1be20dc40e436305f2669
nperezcasas <nupc20@gmail.com>
2024-10-15 18:54:45 -0700

    Update public path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider attribute="class">
       <Head>
-        <link rel="icon" href="/public/favicon.ico" sizes="any" />
+        <link rel="icon" href="/public/beyond-nuria-v2/favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" sizes="32x32" href="/beyond-nuria-v2/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/beyond-nuria-v2/favicon-16x16.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/beyond-nuria-v2/apple-touch-icon.png" />
