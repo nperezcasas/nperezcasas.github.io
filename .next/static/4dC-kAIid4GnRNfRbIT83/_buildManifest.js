c95333ecbc2d16e5f509c2b7216468bc504677f2
nperezcasas <nupc20@gmail.com>
2024-10-15 17:00:08 -0700

    Vercel Modifications

--- .next/static/4dC-kAIid4GnRNfRbIT83/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/672-68a59cef5dc69ea9.js","static/chunks/pages/index-631cf0cbd3421494.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-        unoptimized: true,
-    },
-    assetPrefix: '/beyond-nuria-v2/',
-    basePath: '/beyond-nuria-v2',
-}
-
-module.exports = nextConfig
+    reactStrictMode: true,
+    swcMinify: true,
+  }
+  
+  module.exports = nextConfig
