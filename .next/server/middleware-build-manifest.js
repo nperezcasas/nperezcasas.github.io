6158660bf765ba6e478fafece0abe991108ad40f
nperezcasas <nupc20@gmail.com>
2024-10-16 12:58:14 -0700

    Revert "Fix resume path and update asset references"
    This reverts commit 6fe1615393a7345ee6a177eb891202e84f3f6303.

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,34 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:[],pages:{"/":["static/chunks/webpack-1b3a1ebc71cb266a.js","static/chunks/framework-64ad27b21261a9ce.js","static/chunks/main-df408f72103466a8.js","static/chunks/743-64b583d35446f754.js","static/chunks/pages/index-f852b3e9db3dd261.js"],"/_app":["static/chunks/webpack-1b3a1ebc71cb266a.js","static/chunks/framework-64ad27b21261a9ce.js","static/chunks/main-df408f72103466a8.js","static/css/bfd635a339d1ba5a.css","static/chunks/pages/_app-1e0dc7fe05805827.js"],"/_error":["static/chunks/webpack-1b3a1ebc71cb266a.js","static/chunks/framework-64ad27b21261a9ce.js","static/chunks/main-df408f72103466a8.js","static/chunks/pages/_error-7a92967bea80186d.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [
+    "static/chunks/react-refresh.js"
+  ],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [],
+  "pages": {
+    "/": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/index.js"
+    ],
+    "/_app": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_app.js"
+    ],
+    "/_error": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/_error.js"
+    ]
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
