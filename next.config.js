212eed109d66db8d770ca246fbc70b93d27aab4a
nperezcasas <nperezcasas@blc.edu>
2025-05-31 18:10:54 -0700

    Update configuration for root URL deployment

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
-    basePath: '/beyond-nuria-v2',
-    assetPrefix: '/beyond-nuria-v2/',
     output: 'export',
     images: {
         unoptimized: true,
