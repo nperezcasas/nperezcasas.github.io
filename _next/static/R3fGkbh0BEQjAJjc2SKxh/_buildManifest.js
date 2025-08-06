cb97df54a251fc559d7aef1e4009a7bb472e08e6
nperezcasas <55450564+nperezcasas@users.noreply.github.com>
2025-08-06 20:58:03 +0000

    Deploying to main from @ nperezcasas/nperezcasas.github.io@46c0f28dd1442085f8725df2d149d8c5aec9cf9b 🚀

---
+++ _next/static/R3fGkbh0BEQjAJjc2SKxh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/743-9a9af78269b91921.js","static/chunks/pages/index-bbd5de47375cbedd.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
